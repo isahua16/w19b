7e850a0bd370c15a32ad75d752ef40e5e0fbbd83
isahua16 <isaelhuard@gmail.com>
2023-05-29 14:35:21 -0600

    Added comments

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ def init():
     while(True):
         try:
             print("Welcome to the simple calculator!\nPlease select one of the following options:\nAddition: 1\nSubtraction: 2\nMultiplication: 3\nDivision: 4\nYour Choice:")
-            choice = input()
-            choice = int(choice)
+            choice = int(input())
+            # Get the user's choice and convert it to a number
             if(choice == 1 or choice == 2 or choice == 3 or choice == 4):
+                # Get the user's two numbers they wish to do arithmetic on
                 user_num_one, user_num_two = get_numbers()
                 calculate(choice, user_num_one, user_num_two)
-                if(not restart()):
+                # If the user answers no to restart, get out of the loop and end program.
+                if(restart() == False):
                     return
             else:
                 print("Your entry is not valid. ")
@@ -19,38 +21,47 @@ def init():
 def restart():
     while(True):
         try:
-            print("Would you like to do another calculation?\n 1: Yes\n 2: No\nYour Choice: ")
+            print("Would you like to do another calculation?\nYes: 1\nNo: 2\nYour Choice: ")
+            # Ask for user input and convert it to a number
             user_input = int(input())
             if(user_input == 1):
+                # Will loop the init function
                 return True
             elif(user_input == 2):
                 print("Goodbye")
+                # Will break the init function loop
                 return False
         except ValueError:
             print("Enter 1 or 2")
         except:
-            print("Something is wrong")
+            print("Something went wrong")
     
-
 def get_number():
     while(True):
+        # Will keep asking for a valid number until the user enters a valid number
         try:
             num = float(input())
+            # Return a valid number
             return num
         except ValueError:
             print("Please enter a valid number. Enter a another number: ")
         except:
-            print("Something went wrong, try again")
+            print("Something went wrong.")
 
 def get_numbers():
     print("Enter a first number: ")
+    # Store the first number into a variable
     user_num_one = get_number()
     print("Enter a second number: ")
+    # Store the second number into another variable
     user_num_two = get_number()
+    # Return those two numbers
     return user_num_one, user_num_two
 
 def calculate(choice, num_one, num_two):
+    # Pass the choice and the two numbers into this function
     try:
+        # Return the result based on the user's choice
         if(choice == 1):
             print("Your result:", num_one + num_two)
         elif(choice == 2):
@@ -61,7 +72,10 @@ def calculate(choice, num_one, num_two):
             print("Your result:", num_one / num_two)
     except ZeroDivisionError:
         print("Cannot divide a number by 0")
+    except:
+        print("Something went wrong.")
 
+# Call the program
 init()
 
 
